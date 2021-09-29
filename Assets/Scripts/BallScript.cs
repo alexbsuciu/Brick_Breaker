b9a691c7ed5ca8ad2a31d3f17b6a5001767d35b6
Alex S <alexb.suciu@gmail.com>
2021-09-29 16:21:42 +0300

    Initial commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
+    public Transform Explosion;
+    public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gm.gameOver)
+        {
+            return;
+        }
+        //Muta mingea langa paleta
+        if(!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+        //Release ball
+        if(Input.GetButtonDown("Jump")&& !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+    }
+
+    //Collision cu partea de jos
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Bottom"))
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+            gm.UpdateLives(-1);
+        }
+    }
+
+    //Collision cu caramizile
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.CompareTag("Brick"))
+        {
+            Transform newExplosion = Instantiate(Explosion, other.transform.position, other.transform.rotation);
+            Destroy(newExplosion.gameObject, 2.5f);
+            gm.UpdateScore(other.gameObject.GetComponent<BrickScript>().points);
+            gm.UpdateNumberOfBricks();
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int lives;
+    public int score;
+    public Text livesText;
+    public Text scoreText;
+    public bool gameOver;
+    public GameObject gameOverPanel;
+    public int numberOfBricks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesText.text = "Lives: " + lives;
+        scoreText.text = "Score: " + score;
+        numberOfBricks = GameObject.FindGameObjectsWithTag("Brick").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Modifica nr de vieti
+    public void UpdateLives(int changeInLives)
+    {
+        lives += changeInLives;
+
+        //Check for no lives left/end of game
+        if(lives <=0)
+        {
+            lives = 0;
+            GameOver();
+        }
+
+        livesText.text = "Lives: " + lives;
+    }
+
+    //Modifica scor
+    public void UpdateScore(int points)
+    {
+        score += points;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateNumberOfBricks()
+    {
+        numberOfBricks--;
+        if(numberOfBricks<=0)
+        {
+            GameOver();
+        }
+    }
+
+    //End Game
+    void GameOver()
+    {
+        gameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    //Buton
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
